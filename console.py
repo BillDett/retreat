cf51770d4bae1e3ec348e3c365136bc5ba89de88
Bill Dettelback <billdett@gmail.com>
2017-09-17 06:39:08 -0400

    Downgrade Pins

--- console.py
+++ console.py
@@ -13,6 +13,17 @@ def randomPoint(radius):
 	z = sin(latitude) * radius
 	return (x, y, z)
 
+# Randomly 'downgrade' pins from GOOD->BAD->DESTROY
+def downgradePins(pins, percentage):
+	for p in pins:
+		if random() <= percentage:		# We're going to degrade this Pin
+			if p['status'] == GOOD:
+				p['status'] = BAD
+				p['line'].set_material(pinBadColor)
+			elif p['status'] == BAD:
+				p['status'] = DESTROY
+				p['line'].set_material(pinDestroyColor)
+
 
 DISPLAY = pi3d.Display.create()
 DISPLAY.set_background(0.0,0.0,0.0,1) # Black
@@ -31,14 +42,20 @@ logo = pi3d.ImageSprite(isdlogo, shader)
 ball = pi3d.Sphere(sides=24, slices=24)
 
 # "Pins" around the globe showing the Unobtanium mining operations
+# Each Pin is a tuple of a pi3d.Lines and a status
 pinGoodColor = (129/255, 220/255, 247/255)		# Light Blue
 pinBadColor = (252/255, 106/255, 28/255)		# Dark Orange
 pinDestroyColor = (252/255, 28/255, 28/255)		# Red
+GOOD = 0
+BAD = 1
+DESTROY = 2
 pins = []
-num_pins = 45
+num_pins = 75
+
+# Generate initial set of Pins
 for p in range(num_pins):
-	pin = pi3d.Lines(vertices=[(0.0, 0.0, 0.0), randomPoint(1.25)])
-	pin.set_material(pinDestroyColor)
+	pin = {'line': pi3d.Lines(vertices=[(0.0, 0.0, 0.0), randomPoint(1.25)]), 'status': GOOD}
+	pin['line'].set_material(pinGoodColor)
 	pins.append(pin)
 
 # Speed of planet rotation
@@ -55,10 +72,12 @@ while DISPLAY.loop_running():
 		mykeys.close()
 		DISPLAY.destroy()
 		break
+	elif k == 100:	# 'd'
+		downgradePins(pins, 0.20)
 	logo.position(4.5, 2.0, backplaneZ)
 	logo.draw()
 	ball.draw(shader, [earthimg])
 	ball.rotateIncY(rot)
 	for pin in pins:
-		pin.draw()
-		pin.rotateIncY(rot)
+		pin['line'].draw()
+		pin['line'].rotateIncY(rot)
